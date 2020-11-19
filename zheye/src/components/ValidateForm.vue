301a657be5ccb47a5fbf7b8a248620f6c28410a6
lx812833 <lx812833@163.com>
2020-11-19 22:42:28 +0800

    引入mitt用于事件传递

--- zheye/src/components/ValidateForm.vue
+++ zheye/src/components/ValidateForm.vue
@@ -10,19 +10,39 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onUnmounted } from "vue";
+/**
+ * emitter: 使用 mitt 创建监听器，用于父子组件间通信
+ */
+import mitt from "mitt";
+export const emitter = mitt();
+type validateFunc = () => boolean;
 
 export default defineComponent({
   name: "ValidateForm",
   emits: ["form-submit"],
   setup(props, context) {
+    let funcArr: validateFunc[] = [];
+    emitter.on("form-item-created", val => {
+      funcArr.push(val);
+    });
     const submitForm = () => {
-      context.emit("form-submit", true);
-    }
+      /**
+       * 这里 funcArr 是传递过来的validateInput验证函数，执行后返回Boolean值
+       * 再由map遍历为布尔数组（因要一次性执行多个输入框），再every()
+       */
+      const result = funcArr.map((func) => func()).every((result) => result);
+      context.emit("form-submit", result);
+    };
+    onUnmounted(() => {
+      emitter.all.clear();
+      funcArr = [];
+    });
+
     return {
-      submitForm
-    }
-  }
+      submitForm,
+    };
+  },
 });
 </script>
 
