707c28f29b00ee5d20043f6a53a476925b260cce
lx812833 <lx812833@163.com>
2020-12-06 16:39:30 +0800

    render函数

--- schema-form/src/main.ts
+++ schema-form/src/main.ts
@@ -1,4 +1,24 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp, defineComponent, h } from 'vue';
+import HelloWorld from "@/components/HelloWorld.vue";
+const img = require("./assets/logo.png");
+// import App from "./App.vue";
+
+const App = defineComponent({
+  render() {
+    /**
+     * 节点类型  节点属性  子节点
+     */
+    return h("div", { id: "app" }, [
+      h("img", {
+        alt: "Vue logo",
+        src: img
+      }),
+      h(HelloWorld, {
+        msg: "Welcome to Your Vue.js + TypeScript App",
+        age: 12
+      })
+    ])
+  }
+})
 
 createApp(App).mount("#app");
