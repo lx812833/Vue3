b43348a2be50c0cf0c910ba0e2a9786690f191ee
lx812833 <lx812833@163.com>
2020-12-06 18:01:44 +0800

    vue3中使用jsx

--- schema-form/src/main.ts
+++ schema-form/src/main.ts
@@ -1,24 +1,44 @@
-import { createApp, defineComponent, h } from 'vue';
+import { createApp, defineComponent, h, reactive, ref } from 'vue';
+// import App from "./App.vue";
+import App from "./APP"; // App.tsx
+
+/**
+ * setup 中使用render函数
+ * 
 import HelloWorld from "@/components/HelloWorld.vue";
 const img = require("./assets/logo.png");
-// import App from "./App.vue";
 
 const App = defineComponent({
-  render() {
-    /**
-     * 节点类型  节点属性  子节点
-     */
-    return h("div", { id: "app" }, [
-      h("img", {
-        alt: "Vue logo",
-        src: img
-      }),
-      h(HelloWorld, {
-        msg: "Welcome to Your Vue.js + TypeScript App",
-        age: 12
-      })
-    ])
+  // setup只会在组件渲染时执行一次
+  setup() {
+    const state = reactive({
+      name: "lx"
+    })
+    const numRef = ref<number>(1);
+
+    setInterval(() => {
+      state.name += "1";
+      numRef.value += 1;
+    }, 10000000);
+
+    return () => {
+      const number = numRef.value;
+      // 节点类型  节点属性  子节点（数组）
+      return h("div", { id: "app" }, [
+        h("img", {
+          alt: "Vue logo",
+          src: img
+        }),
+        h(HelloWorld, {
+          msg: "Welcome to Your Vue.js + TypeScript App",
+          age: 12
+        }),
+        h("p", state.name + number)
+      ])
+    }
   }
 })
+*/
+
 
 createApp(App).mount("#app");
