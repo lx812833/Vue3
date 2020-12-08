1eee057b795acea8bdc78753a962e1e36e3af473
lx812833 <lx812833@163.com>
2020-12-08 21:59:15 +0800

    自定义验证关键字

--- schema-form/src/schemaTests/test1.js
+++ schema-form/src/schemaTests/test1.js
@@ -1,14 +1,16 @@
 // Node.js require:
 const Ajv = require("ajv");
+const localize = require("ajv-i18n");
 
 const schema = {
   type: "object",
   properties: {
     name: {
       type: "string",
+      minLength: 10,
       // format: "email", // format只针对于 string 和 number
-      format: "test",
-      minLength: 1
+      // format: "test", // 自定义验证方法
+      // test: false // 自定义验证关键字
     },
     age: {
       type: "number"
@@ -37,10 +39,25 @@ const schema = {
 }
 
 const ajv = new Ajv();
-ajv.addFormat("test", data => {
-  console.log("自定义验证format", data);
-  return data === "haha";
+
+// 自定义验证方法
+// ajv.addFormat("test", data => {
+//   console.log("自定义验证format", data);
+//   return data === "haha";
+// })
+
+// 自定义验证关键字
+ajv.addKeyword("test", {
+  validate(schema, data) {
+    console.log("schema", schema, data);
+    if(schema) {
+      return true;
+    } else {
+      return schema.length === 6;
+    }
+  }
 })
+
 const validate = ajv.compile(schema);
 const valid = validate({
   name: "haha",
@@ -48,4 +65,7 @@ const valid = validate({
   pets: ["red", 12, "pink"],
   isWorker: true
 });
-if (!valid) console.log(validate.errors);
+if (!valid) {
+  localize.zh(validate.errors);
+  console.log("validate.errors", validate.errors);
+}
