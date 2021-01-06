69df3d349f5452c3990b99e1f47ac259365cb04d
lx812833 <lx812833@163.com>
2021-01-06 22:42:21 +0800

    单元测试1

--- schema-form/tests/unit/example.spec.ts
+++ schema-form/tests/unit/example.spec.ts
@@ -1,9 +1,24 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount } from "@vue/test-utils";
+import { defineComponent, h } from 'vue';
+// import HelloWorld from "@/components/HelloWorld.vue";
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
+const HelloWorld = defineComponent({
+  name: "HelloWorld",
+  props: {
+    msg: {
+      type: String
+    }
+  },
+  setup(props) {
+    return () => {
+      return h("div", props.msg);
+    }
+  }
+})
+
+describe("HelloWorld.vue", () => {
+  it("renders props.msg when passed", () => {
+    const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
       props: { msg }
     })
