126db0bbd8cac3139366ca0a4dc4de1f0677f189
longfuyang <lx812833@163.com>
2021-12-29 22:39:39 +0800

    引入全局样式文件、css变量和 mixin

--- mobile-music/src/main.js
+++ mobile-music/src/main.js
@@ -1,6 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-createApp(App).use(store).use(router).mount('#app')
+// 全局样式文件
+import "@/assets/scss/index.scss";
+
+createApp(App).use(store).use(router).mount("#app");
