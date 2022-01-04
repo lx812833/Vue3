994ebd8ccd6df7dba5ad57e7ecb1e7433cf94c92
longfuyang <lx812833@163.com>
2022-01-04 22:23:00 +0800

    设置路由

--- mobile-music/src/router/index.js
+++ mobile-music/src/router/index.js
@@ -1,20 +1,34 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 const routes = [
-  // {
-  //   path: "/about",
-  //   name: "About",
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: function () {
-  //     return import(/* webpackChunkName: "about" */ "../views/About.vue")
-  //   }
-  // }
+  {
+    path: '/',
+    redirect: '/recommend'
+  },
+  {
+    path: "/recommend",
+    name: "Recommend",
+    component: () => import(/* webpackChunkName: "Recommend" */ "../views/recommend.vue")
+  },
+  {
+    path: "/singer",
+    name: "Singer",
+    component: () => import(/* webpackChunkName: "Singer" */ "../views/singer.vue")
+  },
+  {
+    path: "/top-list",
+    name: "TopList",
+    component: () => import(/* webpackChunkName: "TopList" */ "../views/topList.vue")
+  },
+  {
+    path: "/search",
+    name: "Search",
+    component: () => import(/* webpackChunkName: "TopList" */ "../views/search.vue")
+  },
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHashHistory(process.env.BASE_URL),
   routes
 })
 
