6ab13c65b80c29b2174aff2a06560abd7c047669
longfuyang <lx812833@163.com>
2022-03-17 22:56:35 +0800

    播放器雏形

--- mobile-music/src/App.vue
+++ mobile-music/src/App.vue
@@ -2,18 +2,21 @@
 	<m-header />
 	<tab />
 	<router-view></router-view>
+	<player />
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import Header from "@/components/header/header.vue";
 import Tab from "@/components/tab/tab.vue";
+import Player from "./components/player/player.vue";
 
 export default defineComponent({
 	name: "App",
 	components: {
 		MHeader: Header,
-		Tab
+		Tab,
+		Player,
 	},
 });
 </script>
