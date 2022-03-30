d47aaa95f3f3d351028b686c08b82a02b81dc0e4
longfuyang <lx812833@163.com>
2022-03-30 23:03:45 +0800

    播放器进度条拖动事件

--- mobile-music/src/components/player/progressBar.vue
+++ mobile-music/src/components/player/progressBar.vue
@@ -1,8 +1,13 @@
 <template>
-	<div class="progress-bar" ref="progressRef">
+	<div class="progress-bar" ref="progressBarRef">
 		<div class="bar-inner">
-			<div class="progress" ref="progress" :style="progressStyle"></div>
-			<div class="progress-btn-wrapper" :style="btnStyle">
+			<div class="progress" ref="progressRef" :style="progressStyle"></div>
+			<div class="progress-btn-wrapper"
+				:style="btnStyle"
+				@touchstart.prevent="onTouchStart"
+				@touchmove.prevent="onTouchMove"
+				@touchend.prevent="onTouchEnd"
+			>
 				<div class="progress-btn"></div>
 			</div>
 		</div>
@@ -10,7 +15,7 @@
 </template>
 
 <script>
-import { defineComponent, ref, watch, computed, onMounted, nextTick} from "vue";
+import { defineComponent, ref, watch, computed, onMounted, nextTick, reactive, toRefs} from "vue";
 const progressBtnWidth = 16;
 
 export default defineComponent({
@@ -24,8 +29,10 @@ export default defineComponent({
 	setup(props) {
 		// data
 		const offset = ref(0);
-		const progressWidth = ref(0);
 		const progressRef = ref(null);
+		const progressWidth = ref(0);
+		const progressBarRef = ref(null);
+		const touch = reactive({});
 
 		// watch
 		watch(() => props.progress, (newVal) => {
@@ -41,17 +48,40 @@ export default defineComponent({
 			return `transform: translate3d(${offset.value}px, 0, 0)`;
 		});
 
+		// methods
+		const onTouchStart = (e) => {
+			touch.x1 = e.touches[0].pageX;
+			touch.beginWidth = progressRef.value.clientWidth;
+		}
+		const onTouchMove = (e) => {
+			const delta = e.touches[0].pageX - touch.x1;
+			const tempWidth = touch.beginWidth + delta;
+			const progress = Math.min(1, Math.max(tempWidth / progressWidth.value, 0));
+			offset.value = progressWidth.value * progress;
+		}
+		const onTouchEnd = () => {
+			const barWidth = progressWidth.value - progressBtnWidth;
+			const progress = Math.min(progressRef.value.clientWidth / barWidth, 1);
+
+			console.log("end", progress);
+		}
+
 		// mounted
 		onMounted(async () => {
 			await nextTick();
-			progressWidth.value = progressRef.value.clientWidth;
+			progressWidth.value = progressBarRef.value.clientWidth;
 		})
 
 		return {
 			btnStyle,
 			progressRef,
+			progressBarRef,
 			progressWidth,
 			progressStyle,
+			...toRefs(touch),
+			onTouchStart,
+			onTouchMove,
+			onTouchEnd,
 		};
 	},
 });
