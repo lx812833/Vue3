da469073197585b2404f368c2fd1feed5e215c6c
longfuyang <longfuyang@aosign.cn>
2022-08-15 17:33:55 +0800

    fix：修改

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import Components from 'unplugin-vue-components/vite';
-import { VantResolver } from 'unplugin-vue-components/resolvers';
+import Components from "unplugin-vue-components/vite";
+import { VantResolver } from "unplugin-vue-components/resolvers";
 import postcssPxToViewport from "postcss-px-to-viewport";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: "./",
   plugins: [
     vue(),
     Components({
@@ -16,8 +16,8 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: '@',
-        replacement: '/src'
+        find: "@",
+        replacement: "/src"
       }
     ]
   },
@@ -32,13 +32,13 @@ export default defineConfig({
     postcss: {
       plugins: [
         postcssPxToViewport({
-          unitToConvert: 'px', // 要转化的单位
+          unitToConvert: "px", // 要转化的单位
           viewportWidth: 750, // UI设计稿的宽度
           unitPrecision: 6, // 转换后的精度，即小数点位数
-          propList: ['*'], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
-          viewportUnit: 'vw', // 指定需要转换成的视窗单位，默认vw
-          fontViewportUnit: 'vw', // 指定字体需要转换成的视窗单位，默认vw
-          selectorBlackList: ['ignore-'], // 指定不转换为视窗单位的类名，
+          propList: ["*"], // 指定转换的css属性的单位，*代表全部css属性的单位都进行转换
+          viewportUnit: "vw", // 指定需要转换成的视窗单位，默认vw
+          fontViewportUnit: "vw", // 指定字体需要转换成的视窗单位，默认vw
+          selectorBlackList: ["ignore-"], // 指定不转换为视窗单位的类名，
           minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
           mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
           replace: true, // 是否转换后直接更换属性值
@@ -50,7 +50,7 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: 'dist',
-    assetsDir: 'assets'
+    outDir: "dist",
+    assetsDir: "assets"
   }
 })
