673c63038b7322542ff7e101cd439a96bee66b71
longfuyang <longfuyang@aosign.cn>
2022-10-08 15:30:35 +0800

    feat：defineProps 、defineEmits 、 defineExpose

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -6,21 +6,43 @@
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/images/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <h3>{{ number }}</h3>
   </div>
-  <HelloWorld msg="Vite + Vue" />
-  <br>
-  <br>
-  <br>
+  <HelloWorld
+    ref="hello"
+    msg="Vite + Vue"
+    @increase="increaseNumber"
+    @update="updateNumber"
+  />
+  <br />
+  <br />
+  <br />
   <ReactBlock />
 </template>
 
 <script setup>
-import { onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import { useTestStore } from "@/store/test";
 import HelloWorld from "@/components/helloWorld.vue";
 import ReactBlock from "@/components/rectBlock.vue";
 
+const hello = ref(null);
+const number = ref(0);
+
+const increaseNumber = () => {
+  number.value += 1;
+};
+
+const updateNumber = (val) => {
+  number.value = val;
+  
+  const { age, name, count } = hello.value;
+  console.log("hello2", age, name, count);
+};
+
 onMounted(() => {
+  const { age, name, count } = hello.value;
+  console.log("hello1", age, name, count);
   // const testStore = useTestStore();
   // testStore.handleLogin();
 });
@@ -37,5 +59,9 @@ onMounted(() => {
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+h3 {
+  margin: 20px auto;
+  color: aqua;
 }
 </style>
--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -6,10 +6,12 @@
   <p>getters：{{ testStore.fullName }}</p>
 
   <div class="card">
-    <van-button type="primary" @click="count++">主要按钮 {{ count }}</van-button>
+    <van-button type="primary" @click="count++">
+      主要按钮 {{ count }}
+    </van-button>
     <van-button type="success" @click="changeTestAge">成功按钮</van-button>
+    <van-button type="warning" @click="handleUpdate">警告按钮</van-button>
     <!-- <van-button type="default">默认按钮</van-button> -->
-    <!-- <van-button type="warning">警告按钮</van-button> -->
     <!-- <van-button type="danger">危险按钮</van-button> -->
   </div>
 </template>
@@ -19,20 +21,44 @@ import { ref, computed } from "vue";
 import { storeToRefs } from "pinia";
 import { useTestStore } from "@/store/test";
 
-defineProps({
-  msg: String,
+/**
+ * defineProps 、defineEmits 、 defineExpose 和 withDefaults
+ * 这四个宏函数只能在 <script setup> 中使用，无需导入
+ * 
+ * withDefaults：作用是给defineProps绑定默认值的api
+ */
+const props = defineProps({
+  msg: {
+    type: String,
+    required: true,
+  },
 });
+
+const emits = defineEmits(["increase", "update"]);
 
 const testStore = useTestStore();
 // 使用pinia storeToRefs 解构赋值具有响应性
 const { age } = storeToRefs(testStore);
+// 或者直接使用computed来获取store里的值
 const name = computed(() => testStore.name);
 
 const count = ref(0);
 
 const changeTestAge = () => {
+  emits("increase");
   testStore.updateAge();
 };
+
+const handleUpdate = () => {
+  emits("update", 3);
+};
+
+// 把数据导出，供父组件使用，相当于子传父
+defineExpose({
+  age,
+  name,
+  count,
+});
 </script>
 
 <style lang="scss" scoped>
