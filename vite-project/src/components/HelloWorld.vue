b56136185530f0b788de7e42dce9ac6d5ef6f0e2
longfuyang <longfuyang@aosign.cn>
2022-10-09 14:51:27 +0800

    fix：修改注释

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,19 +1,18 @@
 <template>
-  <p>{{ msg }}</p>
-  <p>{{ testStore.age }}</p>
-  <p>storeToRefs：{{ age }}</p>
-  <p>computed：{{ name }}</p>
-  <p>getters：{{ testStore.fullName }}</p>
-
-  <div class="card">
-    <van-button type="primary" @click="count++">
-      主要按钮 {{ count }}
-    </van-button>
-    <van-button type="success" @click="changeTestAge">成功按钮</van-button>
-    <van-button type="warning" @click="handleUpdate">警告按钮</van-button>
-    <!-- <van-button type="default">默认按钮</van-button> -->
-    <!-- <van-button type="danger">危险按钮</van-button> -->
-  </div>
+	<div class="card">
+		<p>{{ msg }}</p>
+		<p>{{ testStore.age }}</p>
+		<p>storeToRefs：{{ age }}</p>
+		<p>computed：{{ name }}</p>
+		<p>getters：{{ testStore.fullName }}</p>
+		<van-button type="primary" @click="count++">
+			主要按钮 {{ count }}
+		</van-button>
+		<van-button type="success" @click="changeTestAge">成功按钮</van-button>
+		<van-button type="warning" @click="handleUpdate">警告按钮</van-button>
+		<!-- <van-button type="default">默认按钮</van-button> -->
+		<!-- <van-button type="danger">危险按钮</van-button> -->
+	</div>
 </template>
 
 <script setup>
@@ -21,50 +20,62 @@ import { ref, computed } from "vue";
 import { storeToRefs } from "pinia";
 import { useTestStore } from "@/store/test";
 
-/**
- * defineProps 、defineEmits 、 defineExpose 和 withDefaults
- * 这四个宏函数只能在 <script setup> 中使用，无需导入
- * 
- * withDefaults：作用是给defineProps绑定默认值的api
- */
-const props = defineProps({
-  msg: {
-    type: String,
-    required: true,
-  },
-});
-
-const emits = defineEmits(["increase", "update"]);
-
 const testStore = useTestStore();
 // 使用pinia storeToRefs 解构赋值具有响应性
 const { age } = storeToRefs(testStore);
 // 或者直接使用computed来获取store里的值
 const name = computed(() => testStore.name);
-
 const count = ref(0);
 
+const props = defineProps({
+	msg: {
+		type: String,
+		required: true,
+	},
+});
+
+const emits = defineEmits(["increase", "update"]);
+
 const changeTestAge = () => {
-  emits("increase");
-  testStore.updateAge();
+	emits("increase");
+	testStore.updateAge();
 };
 
 const handleUpdate = () => {
-  emits("update", 3);
+	emits("update", 3);
 };
 
 // 把数据导出，供父组件使用，相当于子传父
 defineExpose({
-  age,
-  name,
-  count,
+	age,
+	name,
+	count,
 });
 </script>
 
 <style lang="scss" scoped>
 p {
-  margin-bottom: 25px;
-  font-size: $font-size-medium;
-  color: $color-sub-theme;
+	margin-bottom: 25px;
+	font-size: $font-size-medium;
+	color: $color-sub-theme;
 }
 </style>
+
+<!--
+	defineProps 、defineEmits 、 defineExpose 和 withDefaults
+	这四个宏函数只能在 <script setup> 中使用，无需导入
+
+	withDefaults：作用是给defineProps绑定默认值的api
+-->
+
+<!-- <script setup lang="ts">
+interface Props {
+	msg?: string;
+	labels?: string[];
+}
+
+const props = withDefaults(defineProps<Props>(), {
+	msg: "hello vue3!",
+	labels: () => ["one", "two"],
+});
+</script> -->
--- vite-project/src/components/rectBlock.vue
+++ vite-project/src/components/rectBlock.vue
@@ -1,79 +1,85 @@
 <template>
-  <div class="wrap">
-    <div class="card" ref="domRef">
-      <strong>ABCD</strong>
-    </div>
-  </div>
+	<div class="wrap">
+		<div class="card" ref="domRef">
+			<strong>ABCD</strong>
+		</div>
+	</div>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from "vue";
+import { ref, onMounted, computed, onUnmounted } from "vue";
 
 const useMouse = () => {
-  const domRef = ref(null);
-  const mousePos = ref({ x: 0, y: 0 });
-  const rect = ref({ w: 0, h: 0 });
+	const domRef = ref(null);
+	const mousePos = ref({ x: 0, y: 0 });
+	const rect = ref({ w: 0, h: 0 });
 
-  onMounted(() => {
-    rect.value = {
-      w: domRef.value.clientWidth,
-      h: domRef.value.clientHeight,
-    };
+	const movePosition = (e) => {
+		mousePos.value = {
+			x: e.offsetX,
+			y: e.offsetY + 20,
+		};
+	};
 
-    domRef.value.addEventListener("mousemove", (e) => {
-      mousePos.value = {
-        x: e.offsetX,
-        y: e.offsetY + 20,
-      };
-    });
-  });
+	onMounted(() => {
+		rect.value = {
+			w: domRef.value.clientWidth,
+			h: domRef.value.clientHeight,
+		};
 
-  return {
-    rect,
-    domRef,
-    mousePos,
-  };
+		domRef.value.addEventListener("mousemove", movePosition);
+	});
+
+	onUnmounted(() => {
+		domRef.value.removeEventListener("mousemove", movePosition);
+	});
+
+	return {
+		rect,
+		domRef,
+		mousePos,
+	};
 };
 
 const { domRef, mousePos, rect } = useMouse();
 
 const trans = computed(() => {
-  const rotateX = -(mousePos.value.y / rect.value.h) * 1.8;
-  const rotateY = (mousePos.value.x / rect.value.w - 0.5) * 2.2;
-  return `rotateX(${rotateX.toFixed(2)}deg) rotateY(${rotateY.toFixed(2)}deg)`;
+	const rotateX = -(mousePos.value.y / rect.value.h) * 1.8;
+	const rotateY = (mousePos.value.x / rect.value.w - 0.5) * 2.2;
+	return `rotateX(${rotateX.toFixed(2)}deg) rotateY(${rotateY.toFixed(2)}deg)`;
 });
 
 const color = computed(() => {
-  return mousePos.value.x > 100 ? "pink" : "blue";
+	return mousePos.value.x > 100 ? "pink" : "blue";
 });
 </script>
 
 <style lang="scss" scoped>
 .wrap {
-  perspective: 40px;
-  padding-top: 20px;
-  .card {
-    display: grid;
-    width: 20em;
-    height: 12em;
-    margin: 0 auto;
+	perspective: 40px;
+	padding-top: 20px;
+	.card {
+		display: grid;
+		width: 20em;
+		height: 12em;
+		margin: 0 auto;
 
-    transition: transform;
-    transform: v-bind(trans);
-    /**
+		transition: transform;
+		transform: v-bind(trans);
+		/**
       用于设置旋转元素的基点位置
       默认值：transform-origin:50% 50% 0;
       transform-origin属性值可以是百分比、em、px等具体的值，也可以是top、right、bottom、left和center这样的关键词。
     */
-    transform-origin: center 20px;
+		transform-origin: center 20px;
 
-    background: #f3f3f3;
-    box-shadow: 3px 2px 8px 2px #c5c5c59e;
-  }
-  .card strong {
-    place-self: center;
-    font-size: 42px;
-    color: v-bind(color);
-  }
+		background: #f3f3f3;
+		box-shadow: 3px 2px 8px 2px #c5c5c59e;
+	}
+	.card strong {
+		place-self: center;
+		font-size: 42px;
+		color: v-bind(color);
+	}
 }
 </style>
