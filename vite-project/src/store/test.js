e91052e86715902f69f205d7ffca00732a19d7df
longfuyang <longfuyang@aosign.cn>
2022-10-20 15:52:10 +0800

    feat：persist注释

--- vite-project/src/store/test.js
+++ vite-project/src/store/test.js
@@ -5,6 +5,18 @@ import { setToken } from "@/utils/auth";
 export const useTestStore = defineStore("test", {
   /**
    * 开启数据缓存：（可自定义 key、持久化部分 state）
+   * 
+   * persist: {
+      enabled: true，
+      strategies: [
+        {
+          key: 'myCounter', // 存储的 key 值，默认为 storeId
+          storage: localStorage, // 存储的位置，默认为 sessionStorage
+          paths: ['name', 'age'], // 需要存储的 state 状态，默认存储所有的状态
+        }
+      ]
+    }
+   * 
    * https://juejin.cn/post/7049196967770980389
    */
 
