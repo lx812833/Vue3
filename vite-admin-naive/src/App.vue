0874de8de0e132c49f742f20baa7e7ad0af36e01
longfuyang <lx812833@163.com>
2022-12-07 00:05:34 +0800

    build：修改主题色

--- vite-admin-naive/src/App.vue
+++ vite-admin-naive/src/App.vue
@@ -1,11 +1,17 @@
 <template>
-	<n-space m-50>
-		<n-button>Default</n-button>
-		<n-button type="tertiary"> Tertiary </n-button>
-		<n-button type="primary"> Primary </n-button>
-		<n-button type="info"> Info </n-button>
-		<n-button type="success"> Success </n-button>
-		<n-button type="warning"> Warning </n-button>
-		<n-button type="error"> Error </n-button>
-	</n-space>
+	<AppProvider>
+		<n-space m-50>
+			<n-button>Default</n-button>
+			<n-button type="tertiary"> Tertiary </n-button>
+			<n-button type="primary"> Primary </n-button>
+			<n-button type="info"> Info </n-button>
+			<n-button type="success"> Success </n-button>
+			<n-button type="warning"> Warning </n-button>
+			<n-button type="error"> Error </n-button>
+		</n-space>
+	</AppProvider>
 </template>
+
+<script setup>
+import AppProvider from "@/components/AppProvider/index.vue";
+</script>
