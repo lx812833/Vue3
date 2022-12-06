4c900288ed08acc3c013c44c136adbbda734942f
longfuyang <lx812833@163.com>
2022-12-06 23:51:28 +0800

    build：集成unocss

--- vite-admin-naive/build/plugin/index.js
+++ vite-admin-naive/build/plugin/index.js
@@ -4,13 +4,14 @@ import vue from '@vitejs/plugin-vue';
  * * 扩展setup插件，支持在script标签中使用name属性
  * usage: <script setup name="MyComp"></script>
  */
-
+import { unocss } from './unocss';
 import VueSetupExtend from 'vite-plugin-vue-setup-extend';
 
 export function createVitePlugins(viteEnv, isBuild) {
   const plugins = [
     vue(),
     VueSetupExtend(),
+    unocss(),
   ]
 
   return plugins;
