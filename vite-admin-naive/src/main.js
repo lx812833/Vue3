e6d91a929b132b41d776ca12fe8adb37284539d8
longfuyang <lx812833@163.com>
2022-12-07 22:22:36 +0800

    build：pinia集成

--- vite-admin-naive/src/main.js
+++ vite-admin-naive/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue';
 import { setupRouter } from '@/router';
+import { setupStore } from '@/store';
 import '@/styles/index.scss';
 import 'uno.css';
 import App from './App.vue';
 
 const app = createApp(App);
 setupRouter(app);
+setupStore(app);
 
 app.mount('#app');
